0a7b279ee287a69eaef74f6ae6aaf64c024f5bb4
Wiel <yahies.llatas@sinapsiscorp.com>
2023-11-09 12:06:20 -0500

    dww

--- scripts/model_loader.py
+++ scripts/model_loader.py
@@ -7,7 +7,7 @@ from keras.models import load_model
 def cargarModelo():
 
     FILENAME_MODEL_TO_LOAD = "caries_model_full.h5"
-    MODEL_PATH = "../model"
+    MODEL_PATH = "../../model"
 
     # Cargar la RNA desde disco
     loaded_model = load_model(MODEL_PATH + "/" + FILENAME_MODEL_TO_LOAD)
